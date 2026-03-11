6c1ccfb8fb2c99e2a530a1698e008c10c343fc4c
BomDia16 <arturbarbosaoliveira@gmail.com>
2026-03-11 09:10:10 -0300

    Arrumando statistics e centralizando tela

--- FrontEnd/statistics/script.js
+++ FrontEnd/statistics/script.js
@@ -6,19 +6,28 @@ const tempoTotal = document.getElementById('tempoTotal')
 
 const totais = parseInt(tempo1) + parseInt(tempo2) + parseInt(tempo3)
 
-const minutos = totais / 60
-const segundos = totais % 60
+let minutos = totais / 60
+let segundos = totais % 60
+
+if (minutos == null) minutos = 0
+if (segundos == null) segundos = 0
 
 const tempoFormatado = minutos.toFixed(0).padStart(2, "0") + ":" + segundos.toFixed(0).padStart(2, "0");
 
 tempoTotal.innerHTML = tempoFormatado
 
 // Quantidade vezes
-const quantidadeTempo1 = localStorage.getItem('quantidadeTempo1')
-const quantidadeTempo2 = localStorage.getItem('quantidadeTempo2')
-const quantidadeTempo3 = localStorage.getItem('quantidadeTempo3')
+let quantidadeTempo1 = localStorage.getItem('quantidadeTempo1')
+let quantidadeTempo2 = localStorage.getItem('quantidadeTempo2')
+let quantidadeTempo3 = localStorage.getItem('quantidadeTempo3')
 const quantidadeMeditacoes = document.getElementById('quantidadeMeditacoes')
 const meditacaoMais = document.getElementById('meditacaoMais')
+
+if (quantidadeTempo1 == null) quantidadeTempo1 = 0
+
+if (quantidadeTempo2 == null) quantidadeTempo2 = 0
+
+if (quantidadeTempo3 == null) quantidadeTempo3 = 0
 
 quantidadeMeditacoes.innerHTML = parseInt(quantidadeTempo1) + parseInt(quantidadeTempo2) + parseInt(quantidadeTempo3)
 
