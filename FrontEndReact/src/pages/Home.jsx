bb347dfc51d01743fa2c2a70aa2e3542400fa1c4
BomDia16 <arturbarbosaoliveira@gmail.com>
2026-03-17 14:15:23 -0300

    Adicionando a rota Home

---
+++ FrontEndReact/src/pages/Home.jsx
@@ -0,0 +1,11 @@
+function Home() {
+    return (
+        <>
+            <section id="center">
+                <h1 className="text-2xl-rose-800">
+                    Bom dia
+                </h1>
+            </section>
+        </>
+    )
+}
